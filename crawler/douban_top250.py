725314f4385a8395d3e97b0acd1cd22813145295
lihang.shan <lihang.shan@vorteklab.com>
2025-04-11 14:51:35 +0800

    fix：更改更强的解析器

--- crawler/douban_top250.py
+++ crawler/douban_top250.py
@@ -6,7 +6,7 @@ headers = {
 }
 
 response = requests.get("https://movie.douban.com/top250",headers=headers)
-soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(response.text, 'lxml')
 items = soup.find_all('div', class_='item')
 for item in items:
     title = item.find('span', class_='title').text.strip()
